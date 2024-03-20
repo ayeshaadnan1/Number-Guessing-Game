65b7ff854f7a88fad315e72dadced3727e2d8ec0
ayeshaadnan1 <160243600+ayeshaadnan1@users.noreply.github.com>
2024-03-20 13:04:38 +0500

    Add files via upload

---
+++ game.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+let favouriteNumber = 70;
+let guessingNumber = await inquirer.prompt([
+  {
+    message: "Enter Your Number",
+    type: "number",
+    name: "YourGuess",
+  },
+]);
+if (guessingNumber.YourGuess < 70) {
+  console.log("opp's! your Number is too Low ");
+} else if (guessingNumber.YourGuess > 70) {
+  console.log("opp's! your Number is too High ");
+} else if (guessingNumber.YourGuess === 70) {
+  console.log("Congratulation! You Guess Correct Number ");
+} else {
+  console.log("Try Again !!!!!!!!!!");
+}
